75f9df8606cf74a2c48682ecb5615bc5df97a031
Göktuğ Gezer <ggezer780@gmail.com>
2025-09-05 10:55:48 +0300

    made changes ready  for prod env

--- packages/apps/admin/src/App.tsx
+++ packages/apps/admin/src/App.tsx
@@ -1,17 +1,33 @@
 
 import { Button } from '@my/ui-kit'
 import { Input } from '@my/ui-kit'
+import { Outlet, Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
+import Error from './pages/Error'
+
+const Layout = () => (
+  <>
+    <Button>Click me</Button>
+    <Input label='Email' />
+    <div>
+      sdsd
+    </div>
+    Admin App
+    <Outlet />
+  </>
+)
 
 function App() {
-
   return (
     <>
-      <Button>Click me</Button>
-      <Input label='Email' />
-      <div>
-        sdsd
-      </div>
-      Admin App
+      <Routes >
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='dash' element={<Dashboard />} />
+          <Route path='*' element={<Error />} />
+        </Route>
+      </Routes>
     </>
   )
 }
--- packages/apps/main/src/App.tsx
+++ packages/apps/main/src/App.tsx
@@ -1,16 +1,32 @@
 
 import { Button } from '@my/ui-kit'
 import { Input } from '@my/ui-kit'
+import { Outlet, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Error from './pages/Error'
+
+const Layout = () => (
+  <>
+    <Button>Click me</Button>
+    <Input label='Email' />
+
+    Main App
+
+    <Outlet />
+  </>
+)
 
 function App() {
 
   return (
-    <>
-      <Button>Click me</Button>
-      <Input label='Email' />
-
-      Main App
-    </>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='*' element={<Error />} />
+      </Route>
+    </Routes>
   )
 }
 
